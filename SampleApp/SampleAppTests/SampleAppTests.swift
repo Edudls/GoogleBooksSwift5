6cc07f65fd49c046bc4a026b77e44a264ae8d864
Edudls <danieljmonaghan@gmail.com>
2019-04-03 17:01:20 -0500

    Added unit tests

--- SampleApp/SampleAppTests/SampleAppTests.swift
+++ SampleApp/SampleAppTests/SampleAppTests.swift
@@ -19,9 +19,30 @@ class SampleAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testBookSearch() {
+        let promise = expectation(description: "Network call successfully downloads books.")
+        let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=test")!
+        
+        NetworkWorker.sharedInstance.getBooks(url: url) { books in
+            if books.count > 0 {
+                promise.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+    
+    func testImageDownloader() {
+        let promise = expectation(description: "Image data downloads successfully.")
+        let url = URL(string: "https://books.google.com/books/content?id=TvxEyBcwtCYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")!
+        
+        NetworkWorker.sharedInstance.getImage(with: url) { data in
+            if let _ = data {
+                promise.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 2, handler: nil)
     }
 
     func testPerformanceExample() {
